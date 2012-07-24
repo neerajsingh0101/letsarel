0097c4a4213e9014b43ce62d0ac27b750199dd6c
Neeraj Singh <neerajdotname@gmail.com>
2012-07-24 04:48:44 -0400

    home page

--- test/unit/production_house_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ProductionHouseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/unit/production_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ProductionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
